838feaedf4ade951ccc5e44f8ac1b92631518402
esc_codigo10 <esc_codigo10@test.com>
2022-09-27 11:58:46 -0500

    practica28

--- consultas_dept_emp/hugosql.sql
+++ consultas_dept_emp/hugosql.sql
@@ -1,6 +1,6 @@
 -- SHOW databases; /*
-CREATE DATABASE detp_emp;
-USE detp_emp;
+CREATE DATABASE dept_emp;
+USE dept_emp;
 -- DROP DATABASE detp_emp; /*
 CREATE TABLE dept(
 deptno NUMERIC(2) PRIMARY KEY,
@@ -9,3 +9,67 @@ loc VARCHAR(11) NOT NULL);
 -- DESCRIBE dept; /*
 INSERT INTO dept(deptno, dname,loc) 
 VALUES(10,'ACCOUNTING','NEW_YORK');
+INSERT INTO dept(deptno, dname,loc) 
+VALUES(20,'RESEARCH','DALLAS');
+INSERT INTO dept(deptno, dname,loc) 
+VALUES(30,'SALES','CHICAGO');
+INSERT INTO dept(dname,deptno,loc) 
+VALUES('OPERATIONS',40,'BOSTON');
+/*SENTENCIA SELECT
+SELECT * |columna1, columna2,...
+FROM nombre-tabla
+[WHERE condición]
+[GROUP BY columna1,columna2 ...]
+[HAVING condición-selección-grupos]
+[ORDEN BY columna1 [DESC], columna2 [DESC], ...]
+*/
+SELECT *
+FROM dept;
+SELECT loc, dname
+FROM dept;
+SELECT *
+FROM dept
+WHERE deptno<30;
+SELECT loc, dname
+FROM dept
+WHERE deptno <30;
+CREATE TABLE emp(
+empno NUMERIC(4) PRIMARY KEY,
+ename VARCHAR(11) NOT NULL,
+job VARCHAR(13) NOT NULL,
+mgr NUMERIC(4),
+hiredate DATE NOT NULL,
+sal NUMERIC(7,2) NOT NULL,
+comm NUMERIC(7,2),
+deptno NUMERIC(2),
+-- Restricción en emp_deptno_dept_deptno_fk llave foranea de la tabla dept
+CONSTRAINT emp_deptno_detp_deptno_fk FOREIGN KEY (deptno) REFERENCES dept (deptno),
+-- Restrcción en emp_mgr_fk llave forania de la tabla emp 
+CONSTRAINT emp_mgr_fk FOREIGN KEY (mgr) REFERENCES emp (empno));
+DESCRIBE emp;
+INSERT INTO emp
+VALUES(7839, 'KING', 'PRESIDENT', NULL, '2011-11-17', 5000, NULL, 10);
+INSERT INTO emp
+VALUES(7698, 'BLAKE','MANAGER',7839,'2011-05-01',2850,NULL,30);
+INSERT INTO emp
+VALUES(7782,'CLARK','MANAGER',7839,'2011-06-09',2450,NULL,10);
+INSERT INTO emp
+VALUES(7566,'JONES','MANAGER', 7839, '2011-04-02',2975,NULL,20);
+INSERT INTO emp
+VALUES(7654,'MARTIN','SALESMAN',7698, '2011-09-28',1250,1400,30);
+INSERT INTO emp
+VALUES(7499,'ALLEN', 'SALESMAN',7698, '2011-02-20',1600,300, 30);
+INSERT INTO emp
+VALUES(7844,'TURNER','SALESMAN',7698, '2011-09-08',1500,0, 30);
+INSERT INTO emp
+VALUES(7900,'JAMES', 'CLERK',7698, '2011-12-03',950,NULL,30);
+INSERT INTO emp
+VALUES(7521,'WARD','SALESMAN',7698, '2011-02-22',1250,500, 30);
+INSERT INTO emp
+VALUES(7902,'FORD','ANALYST', 7566, '2011-12-03',3000,NULL,20);
+INSERT INTO emp
+VALUES(7369,'SMITH', 'CLERK',7902, '2010-12-17',800,NULL, 20);
+INSERT INTO emp
+VALUES(7788,'SCOTT', 'ANALYST', 7566, '2012-12-09',3000,NULL,20);
+INSERT INTO emp
+VALUES(7876,'ADAMS', 'CLERK', 7788, '2012-01-12',1100,NULL,20);
