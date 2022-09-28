d7b1998dda761417dbfe00ccfc3e8fb6a0ab6829
esc_codigo03 <esc_codigo03@test.com>
2022-09-28 11:47:23 -0500

    basededatoshugo

--- consultas_dept_emp/hugosql.sql
+++ consultas_dept_emp/hugosql.sql
@@ -73,3 +73,84 @@ INSERT INTO emp
 VALUES(7788,'SCOTT', 'ANALYST', 7566, '2012-12-09',3000,NULL,20);
 INSERT INTO emp
 VALUES(7876,'ADAMS', 'CLERK', 7788, '2012-01-12',1100,NULL,20);
+INSERT INTO emp
+VALUES(7934,'MILLER','CLERK',7782, '2012-01-23',1300,NULL,10);
+SELECT * FROM emp;
+SELECT ename, hiredate, sal FROM emp;
+CREATE TABLE salgrade(
+    grade NUMERIC(1) PRIMARY KEY,
+    losal NUMERIC(6,2) NOT NULL,
+    hisal NUMERIC(6,2) NOT NULL
+    );
+DESCRIBE salgrade;
+INSERT INTO salgrade
+VALUES(1,700,1200);
+INSERT INTO salgrade
+VALUES(2,1201,1400);
+INSERT INTO salgrade
+VALUES(3,1401,2000);
+INSERT INTO salgrade
+VALUES(4,2001,3000);
+INSERT INTO salgrade
+VALUES(5,3001,9999);
+DESCRIBE salgrade;
+SELECT *
+FROM salgrade;
+SELECT *
+FROM salgrade
+WHERE losal > 1400
+AND hisal < 4000;
+SELECT ename, sal, sal+300
+FROM emp;
+SELECT ename, sal, 12*sal+100
+FROM emp;
+SELECT ename, sal, 100+sal*12
+FROM emp;
+SELECT ename, sal, 12*(sal+100)
+FROM emp;
+SELECT ename, sal, 1.20*sal
+FROM emp;
+SELECT ename, job, sal, comm
+FROM emp;
+SELECT ename, sal, 12*(sal+100)
+FROM emp
+WHERE ename='KING';
+SELECT ename, 12*sal+comm
+FROM emp
+WHERE ename='KING';
+SELECT ename AS nombre, sal salario
+FROM emp;
+SELECT ename AS nombre, 12*sal AS "Salario Anual"
+FROM emp;
+SELECT CONCAT(ename,' ',job) AS empleado
+FROM emp;
+SELECT CONCAT(ename,' is a ',job) AS "Detalles de Empleo"
+FROM emp;
+SELECT DISTINCT deptno
+FROM emp;
+SELECT distinct deptno, job
+FROM emp;
+SELECT ename,job,deptno
+FROM emp
+WHERE job = 'CLERK';
+SELECT ename, sal, comm
+FROM emp
+WHERE sal <= comm;
+SELECT ename, sal
+FROM emp
+WHERE sal BETWEEN 1000 AND 1500;
+select empno, ename,sal, mgr
+from emp
+where mgr IN(7902,7566,7788);
+SELECT empno,ename,mgr,deptno
+FROM emp
+WHERE ename IN('FORD','ALLEN');
+SELECT ename
+FROM emp
+WHERE ename LIKE 'S%';
+SELECT ename, hiredate
+FROM emp
+WHERE hiredate LIKE '2011%';
+SELECT ename
+FROM emp
+WHERE ename LIKE '_A%';
---
+++ consultas_dept_emp/mi_primer_exportacion.sql
@@ -0,0 +1,76 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema dept_emp
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `dept_emp` ;
+
+-- -----------------------------------------------------
+-- Schema dept_emp
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `dept_emp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `dept_emp` ;
+
+-- -----------------------------------------------------
+-- Table `dept_emp`.`dept`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dept_emp`.`dept` ;
+
+CREATE TABLE IF NOT EXISTS `dept_emp`.`dept` (
+  `deptno` DECIMAL(2,0) NOT NULL,
+  `dname` VARCHAR(12) NULL DEFAULT NULL,
+  `loc` VARCHAR(11) NOT NULL,
+  PRIMARY KEY (`deptno`),
+  UNIQUE INDEX `dname` (`dname` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `dept_emp`.`emp`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dept_emp`.`emp` ;
+
+CREATE TABLE IF NOT EXISTS `dept_emp`.`emp` (
+  `empno` DECIMAL(4,0) NOT NULL,
+  `ename` VARCHAR(11) NOT NULL,
+  `job` VARCHAR(13) NOT NULL,
+  `mgr` DECIMAL(4,0) NULL DEFAULT NULL,
+  `hiredate` DATE NOT NULL,
+  `sal` DECIMAL(7,2) NOT NULL,
+  `comm` DECIMAL(7,2) NULL DEFAULT NULL,
+  `deptno` DECIMAL(2,0) NULL DEFAULT NULL,
+  PRIMARY KEY (`empno`),
+  INDEX `emp_deptno_detp_deptno_fk` (`deptno` ASC) VISIBLE,
+  INDEX `emp_mgr_fk` (`mgr` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `dept_emp`.`salgrade`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dept_emp`.`salgrade` ;
+
+CREATE TABLE IF NOT EXISTS `dept_emp`.`salgrade` (
+  `grade` DECIMAL(1,0) NOT NULL,
+  `losal` DECIMAL(6,2) NOT NULL,
+  `hisal` DECIMAL(6,2) NOT NULL,
+  PRIMARY KEY (`grade`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
