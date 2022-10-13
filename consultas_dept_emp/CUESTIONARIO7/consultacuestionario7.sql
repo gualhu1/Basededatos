05d62d2205f3975035f89b01c4f0444cf85fdf1c
esc_codigo9 <esc-codigo9@test.com>
2022-10-13 12:02:03 -0500

    cuestionario7

---
+++ consultas_dept_emp/CUESTIONARIO7/consultacuestionario7.sql
@@ -0,0 +1,8 @@
+SELECT count(*) FROM producto;
+SELECT * FROM producto ORDER BY Fabricante_idCodigo DESC;
+SELECT MAX(*), MIN(*), AVG(*), COUNT(producto.codigo),
+FROM fabricante;  
+DESCRIBE fabricante;
+
+
+
